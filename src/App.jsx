f1467fe8d168fa1be14b47cc154c2be8de6476b7
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-04-24 17:59:17 +0500

    Added NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden !!!</div>} />
           <Route path="/addquestion" element={<div>Add question</div>} />
+          <Route path="/question/:id" element={<div>Question Page</div>} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,22 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "../Button";
 import cls from "./QuestionCard.module.css";
 
-const QuestionCard = () => {
+const QuestionCard = ({ card }) => {
+  const navigate = useNavigate();
+
   return (
     <div className={cls.card}>
       <div>
-        <div>Level: 1</div>
-        <div>Not Completed</div>
+        <div>Level: {card.Level}</div>
+        <div>{card.completed ? "Completed" : "Not Completed"}</div>
       </div>
 
-      <h5 className={cls.cardTitle}>What is JSX?</h5>
+      <h5 className={cls.cardTitle}>{card.question}</h5>
 
       <div className={cls.cardAnswers}>
         <label>short answer:</label>
-        <p className={cls.cardAnswer}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, ad.</p>
+        <p className={cls.cardAnswer}>{card.answer}</p>
       </div>
 
-      <Button onClick={() => {}}>View</Button>
+      <Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
     </div>
   );
 };
