bcddfd17dbb77054f46e2619e3497ee77d5a5e34
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-04-24 14:05:04 +0500

    Added buttons

--- src/Button.jsx
+++
@@ -1,9 +0,0 @@
-const inlineStyles = {
-    color : "lightsalmon",
-}
-
-
-export const Button = (props) =>{
-    console.log(props)
-    return <button className="btn" style={inlineStyles}  onClick={props.onClick}>custom button</button>
-}
