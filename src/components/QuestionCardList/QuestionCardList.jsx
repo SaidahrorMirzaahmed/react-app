df639cc87ce9b04850b67bf7de252290c17dc729
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-04-25 17:18:43 +0500

    Memo

--- src/components/QuestionCardList/QuestionCardList.jsx
+++ src/components/QuestionCardList/QuestionCardList.jsx
@@ -1,7 +1,8 @@
+import { memo } from "react";
 import QuestionCard from "../QuestionCard/QuestionCard";
 import cls from "./QuestionCardList.module.css";
 
-const QuestionCardList = ({ cards }) => {
+export const QuestionCardList = memo(({ cards }) => {
   return (
     <div className={cls.cardList}>
       {cards.map((card, index) => {
@@ -9,6 +10,4 @@ const QuestionCardList = ({ cards }) => {
       })}
     </div>
   );
-};
-
-export default QuestionCardList;
+});
