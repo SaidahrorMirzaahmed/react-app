ecda0adfcdeb7bdc0d0316ea67d6d7c89a149465
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-05-01 16:42:59 +0500

    react toasts

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,20 +1,25 @@
 import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.css";
 import { Header } from "../Header/Header";
+import { ToastContainer } from "react-toastify";
 
 export const MainLayout = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className={cls.mainLayout}>
-      <Header />
-      <div className={cls.mainWrapper}>
-        <Outlet />
-        <footer className={cls.footer}>
-          React Question Cards Application | {currentYear} <br />
-          by Sayidahror Mirzaakhmedov
-        </footer>
+    <>
+      <div className={cls.mainLayout}>
+        <Header />
+        <div className={cls.mainWrapper}>
+          <Outlet />
+          <footer className={cls.footer}>
+            React Question Cards Application | {currentYear} <br />
+            by Sayidahror Mirzaakhmedov
+          </footer>
+        </div>
       </div>
-    </div>
+
+      <ToastContainer />
+    </>
   );
 };
