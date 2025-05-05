5a8c49f58931c197e830c17c4f03acd3c2d1f3d5
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-05-05 14:59:11 +0500

    Added lazy loading

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.css";
 import { Header } from "../Header/Header";
 import { ToastContainer } from "react-toastify";
+import { Suspense } from "react";
+import { Loader } from "../Loader";
 
 export const MainLayout = () => {
   const currentYear = new Date().getFullYear();
@@ -11,7 +13,10 @@ export const MainLayout = () => {
       <div className={cls.mainLayout}>
         <Header />
         <div className={cls.mainWrapper}>
-          <Outlet />
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+
           <footer className={cls.footer}>
             React Question Cards Application | {currentYear} <br />
             by Sayidahror Mirzaakhmedov
