24d80e86b7ca57828f3c6ee92b3f5a08e60ea364
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2025-05-06 16:12:12 +0500

    Theme Context

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { EditQuestionPageLazy } from "./pages/EditQuestionPage";
 import { AuthProvider } from "./auth/AuthProvider.jsx";
 import { useAuth } from "./hooks/useAuth.js";
 import { ForbiddenPage } from "./pages/ForbiddenPage/ForbiddenPage.jsx";
+import { ThemeProvider } from "./theme/ThemeProvider.jsx";
 
 const ProtectedRoutes = () => {
   const { isAuth } = useAuth();
@@ -20,24 +21,26 @@ const ProtectedRoutes = () => {
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/forbidden" element={<ForbiddenPage />} />
-            <Route path="/question/:id" element={<QuestionPage />} />
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/forbidden" element={<ForbiddenPage />} />
+              <Route path="/question/:id" element={<QuestionPage />} />
 
-            <Route element={<ProtectedRoutes />}>
-              <Route path="/addquestion" element={<AddQuestionPageLazy />} />
-              <Route path="/editquestion/:id" element={<EditQuestionPageLazy />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/addquestion" element={<AddQuestionPageLazy />} />
+                <Route path="/editquestion/:id" element={<EditQuestionPageLazy />} />
+              </Route>
+
+              <Route path="*" element={<NotFoundPage />} />
             </Route>
-
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,10 @@
+import { Children, createContext, useState } from "react";
+import { THEME_STORAGE } from "../constants";
+
+export const ThemeContext = createContext(null);
+
+export const ThemeProvider = ({ children }) => {
+  const savedTheme = localStorage.getItem(THEME_STORAGE) || "light";
+  const [theme, setTheme] = useState(savedTheme);
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+};
